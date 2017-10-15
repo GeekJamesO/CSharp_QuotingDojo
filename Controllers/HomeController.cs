99c4559463efccd1d3693849bf9260ffd1612d4f
JamesORourke <geekjamesorourkejr@icloud.com>
2017-10-15 14:44:51 -0700

    Talking to DB

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using QuotingDojo.Connections;
 
 namespace QuotingDojo.Controllers
 {
     public class HomeController : Controller
     {
+        private MySqlConnector ctx;
+        public HomeController()
+        {
+            ctx = new MySqlConnector();
+        }
         // GET: /Home/
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
+            string query = "SELECT * FROM  QuotesTable";
+            var result = ctx.Query(query);
+            ViewBag.allQuotes = result;
             return View();
         }
     }
